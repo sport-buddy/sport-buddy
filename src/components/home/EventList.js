f98ae0241b7b79de8934bf901368877f2f6d845f
budaman <budamantvydas@gmail.com>
2018-10-07 12:20:44 +0300

    maping events

--- src/components/home/EventList.js
+++ src/components/home/EventList.js
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 
 class SportFilters extends Component {
   render() {
-    return <div>{this.props.filterLocations.lt}</div>;
+    console.log(this.props.filterLocations.data);
+
+    const showEvents = () => {
+      let eventList = [];
+      const events = this.props.filterLocations.data.map(location => {
+        location.events.map(event => {
+          if (event.length !== 0) {
+            console.log(event);
+            eventList = [...eventList, event];
+          }
+        });
+      });
+      return eventList.map(l => {
+        return <div key={l.id}>{l.name}</div>;
+      });
+    };
+
+    showEvents();
+
+    return (
+      <div>
+        <h4>{this.props.filterLocations.lt}</h4>
+        {showEvents()}
+      </div>
+    );
   }
 }
 
